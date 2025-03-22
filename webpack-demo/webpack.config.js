e55069d113ec35b4b8bf9419e9703732bc2f54c2
nmalysheva99@mail.ru <nathalie_malsh@LAPTOP-HHAP2AP9>
2025-03-22 15:44:33 +0300

    edit webpack config

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,16 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'My App',
-        }),
-    ],
-    mode: 'development',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My App',
+    }),
+  ],
 };
